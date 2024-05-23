3129a1bf4a047e9e0fcc064d0944c729dffc7f37
KristianMedeshi <kristianmedeshi@gmail.com>
2024-05-23 11:15:59 +0300

    first commit

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,16 @@
+import * as express from 'express';
+import { UsersController } from '../controllers/userController';
+import upload from '../middleware/uploadMiddleware';
+import authMiddleware from '../middleware/authMiddleware';
+
+const userRoutes = express();
+
+userRoutes.post('/sign-up', UsersController.register);
+userRoutes.post('/sign-in', UsersController.login);
+userRoutes.post('/log-out', UsersController.logout);
+userRoutes.get('/auth', authMiddleware, (req, res) => { res.status(200).json({ user: req.user }); });
+
+userRoutes.get('/profile', authMiddleware, UsersController.getProfile);
+userRoutes.patch('/profile', authMiddleware, upload.single('image'), UsersController.updateProfile);
+
+export default userRoutes;
